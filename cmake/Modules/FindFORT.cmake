49fc2aa1347c243473d5ecd3944f06517f623721
Oblitorum <oblitorum@gmail.com>
2020-09-22 03:42:26 +0800

    add compile configs for libfort library

---
+++ cmake/Modules/FindFORT.cmake
@@ -0,0 +1,24 @@
+#
+# Try to find libfort.
+#
+
+# Try to find the header
+find_path(FORT_INCLUDE_DIR fort.h)
+
+# Try to find the library
+find_library(FORT_LIBRARY libfort)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(fort
+  DEFAULT_MSG
+  FORT_INCLUDE_DIR
+  FORT_LIBRARY
+)
+
+mark_as_advanced(
+  FORT_INCLUDE_DIR
+  FORT_LIBRARY
+)
+
+set(FORT_INCLUDE_DIRS ${FORT_INCLUDE_DIR})
+set(FORT_LIBRARIES ${FORT_LIBRARY})
