9cc01cc541c207878445b7ffae623fac507c98d3
Jason <oblitorum@gmail.com>
2020-10-27 20:12:31 +0800

    Revert "UDLD: Refine and modernize the code."
    This reverts commit 7d6c2998780156e1fd29ce0bcb4f66c60d016d66.

--- print-udld.c
+++ print-udld.c
@@ -25,13 +25,11 @@
 
 #include "netdissect-stdinc.h"
 
-#define ND_LONGJMP_FROM_TCHECK
 #include "netdissect.h"
 #include "extract.h"
 
 
 #define UDLD_HEADER_LEN			4
-#define UDLD_TLV_HEADER_LEN		4
 #define UDLD_DEVICE_ID_TLV		0x0001
 #define UDLD_PORT_ID_TLV		0x0002
 #define UDLD_ECHO_TLV			0x0003
@@ -101,68 +99,60 @@ static const struct tok udld_flags_bitmap_str[] = {
 #define	UDLD_EXTRACT_OPCODE(x) ((x)&0x1f)
 
 void
-udld_print(netdissect_options *ndo,
-           const u_char *tptr, u_int length)
+udld_print(netdissect_options *ndo, const u_char *pptr, u_int length)
 {
-    uint8_t ver, code, flags;
+    int code, type, len;
+    const u_char *tptr;
 
     ndo->ndo_protocol = "udld";
     if (length < UDLD_HEADER_LEN)
-        goto invalid;
+        goto trunc;
 
-    ver = UDLD_EXTRACT_VERSION(GET_U_1(tptr));
+    tptr = pptr;
+
+    ND_TCHECK_LEN(tptr, UDLD_HEADER_LEN);
+
     code = UDLD_EXTRACT_OPCODE(GET_U_1(tptr));
-    tptr += 1;
-    length -= 1;
-
-    flags = GET_U_1(tptr);
-    tptr += 1;
-    length -= 1;
 
     ND_PRINT("UDLDv%u, Code %s (%x), Flags [%s] (0x%02x), length %u",
-           ver,
+           UDLD_EXTRACT_VERSION(GET_U_1(tptr)),
            tok2str(udld_code_values, "Reserved", code),
            code,
-           bittok2str(udld_flags_bitmap_str, "none", flags),
-           flags,
-           length + 2);
+           bittok2str(udld_flags_bitmap_str, "none", GET_U_1((tptr + 1))),
+           GET_U_1((tptr + 1)),
+           length);
 
     /*
      * In non-verbose mode, just print version and opcode type
      */
     if (ndo->ndo_vflag < 1) {
-        goto tcheck_remainder;
+	return;
     }
 
-    ND_PRINT("\n\tChecksum 0x%04x (unverified)", GET_BE_U_2(tptr));
-    tptr += 2;
-    length -= 2;
+    ND_PRINT("\n\tChecksum 0x%04x (unverified)", GET_BE_U_2(tptr + 2));
 
-    while (length) {
-        uint16_t type, len;
+    tptr += UDLD_HEADER_LEN;
 
-        if (length < UDLD_TLV_HEADER_LEN)
-            goto invalid;
+    while (tptr < (pptr+length)) {
 
 	type = GET_BE_U_2(tptr);
-        tptr += 2;
-        length -= 2;
-
-        len  = GET_BE_U_2(tptr);
-        tptr += 2;
-        length -= 2;
+        len  = GET_BE_U_2(tptr + 2);
 
         ND_PRINT("\n\t%s (0x%04x) TLV, length %u",
                tok2str(udld_tlv_values, "Unknown", type),
                type, len);
 
-        /* infinite loop check */
-        if (len <= UDLD_TLV_HEADER_LEN)
+        if (type == 0)
             goto invalid;
 
-        len -= UDLD_TLV_HEADER_LEN;
-        if (length < len)
+        /* infinite loop check */
+        if (len <= 4)
             goto invalid;
+
+        len -= 4;
+        tptr += 4;
+
+        ND_TCHECK_LEN(tptr, len);
 
         switch (type) {
         case UDLD_DEVICE_ID_TLV:
@@ -191,17 +181,16 @@ udld_print(netdissect_options *ndo,
             break;
 
         default:
-            ND_TCHECK_LEN(tptr, len);
             break;
         }
         tptr += len;
-        length -= len;
     }
 
     return;
 
 invalid:
     nd_print_invalid(ndo);
-tcheck_remainder:
-    ND_TCHECK_LEN(tptr, length);
+    return;
+trunc:
+    nd_print_trunc(ndo);
 }
